a1c3442b3a11948334b6bea91311fc0dbc87f1d3
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2016-11-10 22:42:39 +0530

    files modified

--- go-workspace/src/github.com/hygull/2_sources04nov2016/templating/templating/images_display/display_images.go
+++ go-workspace/src/github.com/hygull/2_sources04nov2016/templating/templating/images_display/display_images.go
@@ -1,52 +1,125 @@
--- MySQL dump 10.13  Distrib 5.7.15, for osx10.11 (x86_64)
---
--- Host: localhost    Database: media
--- ------------------------------------------------------
--- Server version	5.7.15
+package main
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/kataras/iris"
+)
 
---
--- Table structure for table `images`
---
+func ShowImages(ctx *iris.Context) {
+	/*****/
+	ctx.SetContentType("text/html")
+	fmt.Println("Connecting to DB...")
+	db, err := sql.Open("mysql", "root:admin@321@tcp(127.0.0.1:3306)/media")
 
-DROP TABLE IF EXISTS `images`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `images` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` text NOT NULL,
-  `url` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+	if err != nil {
+		fmt.Println("Error in connection...")
+		fmt.Println(err.Error())
 
---
--- Dumping data for table `images`
---
+		return
+	}
+	defer db.Close()
 
-LOCK TABLES `images` WRITE;
-/*!40000 ALTER TABLE `images` DISABLE KEYS */;
-INSERT INTO `images` VALUES (1,'Admin','https://cdn3.iconfinder.com/data/icons/users-6/100/4-128.png'),(2,'Laptop','https://cdn1.iconfinder.com/data/icons/technology-and-hardware-2/200/vector_66_02-128.png'),(3,'Apple','https://cdn2.iconfinder.com/data/icons/social-icons-color/512/apple-128.png'),(4,'Android','https://cdn0.iconfinder.com/data/icons/flat-round-system/512/android-128.png'),(5,'Coconut','https://cdn3.iconfinder.com/data/icons/pix-glyph-set/50/520768-palm_tree-128.png'),(6,'Tiger','https://cdn3.iconfinder.com/data/icons/animal-heads-and-faces/30/tiger-cartoon-128.png'),(7,'Zebra','https://cdn2.iconfinder.com/data/icons/african-animals/128/zebra-256.png'),(8,'Frog','https://cdn1.iconfinder.com/data/icons/the-most-popular-aquarium-inhabitants-in-glyph-sty/48/frog-128.png');
-/*!40000 ALTER TABLE `images` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Error in connection test...")
+		fmt.Println(err.Error())
+		return
+	}
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+	queryString := "select title, url from images;"
 
--- Dump completed on 2016-11-10 22:22:10
+	fmt.Println(queryString)
+	rows, err := db.Query(queryString)
+	if err != nil {
+		fmt.Println("Error in query execution")
+		return
+	}
+	found := false
+	htmlText := "<!doctype html><body><center>"
+	for rows.Next() {
+		var title, url string
+
+		rows.Scan(&title, &url)
+		htmlText += "<h3 style='color:green'>" + title + "</h3>" +
+			"<img  src='" + url + "' height='10%' width='10%'/><br>"
+		fmt.Println(htmlText)
+		found = true
+	}
+	htmlText += "</center></body>"
+
+	if !found {
+		fmt.Println("No posts found...")
+	}
+
+	ctx.HTML(iris.StatusOK, htmlText)
+}
+
+func ShowImagesAsJSON(ctx *iris.Context) {
+	/*****/
+	ctx.SetContentType("text/html")
+	fmt.Println("Connecting to DB...")
+	db, err := sql.Open("mysql", "root:admin@321@tcp(127.0.0.1:3306)/media")
+
+	if err != nil {
+		fmt.Println("Error in connection...")
+		fmt.Println(err.Error())
+
+		return
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Error in connection test...")
+		fmt.Println(err.Error())
+		return
+	}
+
+	queryString := "select title, url from images;"
+
+	fmt.Println(queryString)
+	rows, err := db.Query(queryString)
+	if err != nil {
+		fmt.Println("Error in query execution")
+		return
+	}
+	found := false
+
+	type Image struct {
+		Title string `json:"title"`
+		Url   string `json:"url"`
+	}
+	var imagesArr []Image
+	//imagesMap := map[string]interface{}{}
+	for rows.Next() {
+		var title, url string
+		rows.Scan(&title, &url)
+		imagesArr = append(imagesArr, Image{title, url})
+		found = true
+	}
+
+	if !found {
+		fmt.Println("No posts found...")
+	}
+	//imagesMap["images"] = imagesArr
+	ctx.JSON(200, iris.Map{"images": imagesArr})
+}
+
+func main() {
+	iris.Get("/v1/images/all", ShowImages)
+	iris.Get("/v1/images/as_json", ShowImagesAsJSON)
+
+	iris.OnError(iris.StatusNotFound, func(ctx *iris.Context) {
+		ctx.SetContentType("text/html")
+		ctx.Write("<body bgcolor='black'><center><h1 style='color:lightgreen;'>SynkkU</h1><h2 style='padding:50px;color:white'>Page not found</h2></center></body>")
+		ctx.Log("%s", "Page not found...404 Error")
+	})
+
+	iris.OnError(iris.StatusInternalServerError, func(ctx *iris.Context) {
+		ctx.Write("<body bgcolor='black'><center><h1 style='padding:330px;color:white'>Internal server error</h1></center></body>")
+		ctx.Log("%s", "Internal server error")
+	})
+	iris.Listen(":8080")
+}
